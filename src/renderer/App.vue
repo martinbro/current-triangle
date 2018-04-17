89a53d7028b3bd33b50f837728b05d7566f1cc8b
Martin Bro Mikkelsen <martinbromikkelsen@gmail.com>
2018-04-17 08:11:14 +0200

    Seneste ændring

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <v-app dark>
+    <v-app light>
       <v-navigation-drawer
         fixed
         :mini-variant="miniVariant"
@@ -9,7 +9,7 @@
         app
       >
         <v-list>
-          <v-list-tile 
+          <v-list-tile
             router
             :to="item.to"
             :key="i"
@@ -27,7 +27,7 @@
       </v-navigation-drawer>
       <v-toolbar fixed app :clipped-left="clipped">
         <v-toolbar-side-icon @click.native.stop="drawer = !drawer"></v-toolbar-side-icon>
-        <v-btn 
+        <v-btn
           icon
           @click.native.stop="miniVariant = !miniVariant"
         >
@@ -46,12 +46,7 @@
           <v-icon>remove</v-icon>
         </v-btn>
         <v-toolbar-title v-text="title"></v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-btn
-          icon
-          @click.native.stop="rightDrawer = !rightDrawer"
-        >
-          <v-icon>menu</v-icon>
+        
         </v-btn>
       </v-toolbar>
       <v-content>
@@ -61,22 +56,6 @@
           </v-slide-y-transition>
         </v-container>
       </v-content>
-      <v-navigation-drawer
-        temporary
-        fixed
-        :right="right"
-        v-model="rightDrawer"
-        app
-      >
-        <v-list>
-          <v-list-tile @click.native="right = !right">
-            <v-list-tile-action>
-              <v-icon light>compare_arrows</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-          </v-list-tile>
-        </v-list>
-      </v-navigation-drawer>
       <v-footer :fixed="fixed" app>
         <v-spacer></v-spacer>
         <span>&copy; 2017</span>
